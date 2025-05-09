7ea3fe09e14dcb46512cfed06e1bc7fa347778e0
snakee02 <daniel.ifv02@gmail.com>
2025-05-09 12:39:35 -0400

    nuevo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
-        <Link className="navbar-brand" to="/">MediBook</Link>
+        <Link className="navbar-brand" to="/">
+          <img src="medibooklogo.png" className="logo-img"/> 
+        </Link>
         <div>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row">
             <li className="nav-item mx-2">
