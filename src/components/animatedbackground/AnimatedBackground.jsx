c438b5cd3773e0f631306b7bd2280592b0fdba18
snakee02 <daniel.ifv02@gmail.com>
2025-05-15 13:14:53 -0400

    cambio de fondo dinámico

--- src/components/animatedbackground/AnimatedBackground.jsx
+++ src/components/animatedbackground/AnimatedBackground.jsx
@@ -2,16 +2,15 @@ import '../animatedbackground/AnimatedBackground.css'
 
 function AnimatedBackground() {
   return (
-    <div className="animated-background">
-      <div className="circle small shade1"></div>
-      <div className="circle medium shade2"></div>
-      <div className="circle medium shade3" style={{ left: '70vw', bottom: '10vh' }}></div>
-      <div className="circle medium shade3" style={{ left: '70vw', bottom: '10vh' }}></div>
-      <div className="circle medium shade3" style={{ left: '70vw', bottom: '10vh' }}></div>
-      <div className="circle small shade2" style={{ left: '20vw', bottom: '50vh' }}></div>
-      <div className="circle small shade2" style={{ left: '20vw', bottom: '50vh' }}></div>
+    <div className="area">
+    <div className="context">
+      <h1>Bienvenido a Medibook</h1>
     </div>
-
+    <ul className="circles">
+      <li></li><li></li><li></li><li></li><li></li>
+      <li></li><li></li><li></li><li></li><li></li>
+    </ul>
+  </div>
   );
 
 }
